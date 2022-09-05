81e5b74ad42361dd1dafff1463c2d8631b331aea
tynan.liu <tynan.liu@xinjifamily.com>
2022-09-05 16:52:02 +0800

    paxos tease

--- paxos/multi-paxos/scripts/frames/overview.js
+++ paxos/multi-paxos/scripts/frames/overview.js
@@ -12,16 +12,15 @@ define(["../../../core/model/log_entry"], function (LogEntry) {
             wait = function() { var self = this; model().controls.show(function() { player.play(); self.stop(); }); };
 
         frame.after(1, function () {
-            model().nodeLabelVisible = false;
             model().clear();
             layout.invalidate();
         })
             .after(1000, function () {
-                frame.model().title = '<h2 style="visibility:visible">Members of the role</h2>'
+                frame.model().title = '<h2 style="visibility:visible">Algorithm Overview</h2>'
                     + '<br/>' + frame.model().controls.html();
                 layout.invalidate();
             })
-            .after(1000, function () {
+            .after(500, function () {
                 frame.model().controls.show();
             })
             .after(1, function () {
@@ -29,95 +28,44 @@ define(["../../../core/model/log_entry"], function (LogEntry) {
                 layout.invalidate();
             })
 
-            .after(500, function () {
-                model().nodes.create("a");
-                layout.invalidate();
-            })
-
-            .after(100, function () {
+            .after(10, function () {
                 frame.snapshot();
-                model().subtitle = '<h2>Paxos defines three member roles:</h2>'
-                    + model().controls.html();
-                layout.invalidate();
-            })
-            .after(100, wait).indefinite()
-            .after(100, function () {
-                frame.snapshot();
-                model().zoom([node("a")]);
-                node("a")._state = "proposer";
-                model().subtitle = '<h2>The <em>Proposer</em></h2>'
-                    + '<h2 id="desc" style="visibility:hidden;">Proposer is used to make <em>Proposal</em> on behalf of clients.</h2>'
-                    + model().controls.html();
-                layout.invalidate();
-            })
-            .after(800, function () {
-                layout.fadeIn($(".subtitle #desc"));
-            })
-            .after(100, wait).indefinite()
-
-            .after(100, function () {
-                frame.snapshot();
-                node("a")._state = "acceptor";
-                model().subtitle = '<h2>The <em>Acceptor</em></h2>'
-                    + '<h2 id="desc" style="visibility:hidden;">Acceptors are used to decide on <em>Proposal</em>.</h2>'
-                    + model().controls.html();
-                layout.invalidate();
-            })
-            .after(800, function () {
-                layout.fadeIn($(".subtitle #desc"));
-            })
-            .after(100, wait).indefinite()
-
-            .after(100, function () {
-                frame.snapshot();
-                node("a")._state = "learner";
-                model().subtitle = '<h2>or the <em>Learner</em></h2>'
-                    + '<h3 id="desc" style="visibility:hidden;">Learner is used to record <em>Proposal</em> on which consensus is reached, and input it into the state machine.</h3>'
-                    + model().controls.html();
-                layout.invalidate();
-            })
-            .after(800, function () {
-                layout.fadeIn($(".subtitle #desc"));
-            })
-            .after(100, wait).indefinite()
-
-            .after(300, function () {
-                frame.snapshot();
-                model().zoom(null);
-                model().subtitle = '<h2>But here\'s the important thing, Learner did not participate in Paxos\' negotiation program.</h2>'
-                    + model().controls.html();
-                layout.invalidate();
-            })
-            .after(100, wait).indefinite()
-
-            .after(300, function () {
                 frame.model().clear();
-                frame.model().title = '<h2 style="visibility:visible">The negotiation program</h2>'
-                    + '<br/>' + frame.model().controls.html();
+
+                model().nodes.create("A");
+                node("A")._state = "acceptor";
+                node("A")._proposalNo = 0;
+
+                model().nodes.create("B");
+                node("B")._state = "acceptor";
+                node("B")._proposalNo = 0;
+
+                model().nodes.create("C");
+                node("C")._state = "acceptor";
+                node("C")._proposalNo = 0;
+            })
+            .after(10, function () {
+                model().subtitle = '<h3>As we imagine, after running a round of Prepare phase, subsequent negotiations can directly enter the Accept phase.</h3>'
+                    + model().controls.html();
                 layout.invalidate();
             })
+            .after(100, wait).indefinite()
 
-            .after(800, function () {
-                frame.model().controls.show();
+            .after(10, function () {
+                frame.snapshot();
+                model().nodes.create("P1");
+                node("P1")._state = "proposer";
+                node("P1")._proposalNo = 0;
+
+                frame.model().clients.create("X");
             })
-
-            .after(1000, function () {
-                frame.model().title = '<h2>Paxos\' negotiation program is divided into three stages.</h2>'
-                    + '<h3 id="prepare" style="visibility:hidden;">Prepare Phase: Negotiate ProposalNo and obtain Proposal that may reach consensus in the previous round.</h3>'
-                    + '<h3 id="accept" style="visibility:hidden;">Accept Phase: Really negotiate the Proposal and broadcast the Proposal to all Acceptors.</h3>'
-                    + '<h3 id="accept" style="visibility:hidden;">Learn Phase: Learn reach consensus Proposal and input it into the state machine.</h3>'
-                    + '<br/>' + frame.model().controls.html();
+            .after(10, function () {
+                model().subtitle = '<h2>zzzzzzzzzzzzzzzzzzz.</h2>'
+                    + model().controls.html();
                 layout.invalidate();
             })
-            .after(1000, function () {
-                layout.fadeIn($(".title #prepare"));
-            })
-            .after(1000, function () {
-                layout.fadeIn($(".title #accept"));
-            })
-            .after(800, function () {
-                frame.model().controls.show();
-            })
+            .after(100, wait).indefinite()
+
 
             .after(50, function () {
                 frame.model().title = frame.model().subtitle = "";
--- paxos/multi-paxos/scripts/frames/tease.js
+++ paxos/multi-paxos/scripts/frames/tease.js
@@ -45,6 +45,7 @@ define(["../../../core/model/log_entry"], function (Proposal) {
             // Livelock
             //------------------------------
             .after(500, function () {
+                frame.snapshot();
                 frame.model().clear();
                 frame.model().title = '<h2 style="visibility:visible">Livelock</h2>'
                     + '<br/>' + frame.model().controls.html();
@@ -54,19 +55,12 @@ define(["../../../core/model/log_entry"], function (Proposal) {
                 frame.model().controls.show();
             })
             .after(10, function () {
+                frame.snapshot();
                 frame.model().clear();
 
                 model().nodes.create("A");
                 node("A")._state = "acceptor";
                 node("A")._proposalNo = 0;
-
-                model().nodes.create("P1");
-                node("P1")._state = "proposer";
-                node("P1")._proposalNo = 0;
-
-                model().nodes.create("P2");
-                node("P2")._state = "proposer";
-                node("P2")._proposalNo = 0;
 
                 model().nodes.create("B");
                 node("B")._state = "acceptor";
@@ -75,16 +69,211 @@ define(["../../../core/model/log_entry"], function (Proposal) {
                 model().nodes.create("C");
                 node("C")._state = "acceptor";
                 node("C")._proposalNo = 0;
-
-                frame.model().clients.create("X");
-                frame.model().clients.create("Y");
-                layout.invalidate();
-                client("X")._value = "X"
-                client("Y")._value = "Y"
             })
             .after(10, function () {
-                model().subtitle = '<h2>ssssssssssssssssssss.</h2>'
+                model().subtitle = '<h3>Livelock means that when multiple proposers negotiate proposals at the same time, they interfere with each other, so that no consensus can be reached on any proposal.</h3>'
                     + model().controls.html();
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().nodes.create("P1");
+                node("P1")._state = "proposer";
+                node("P1")._proposalNo = 0;
+
+                model().nodes.create("P2");
+                node("P2")._state = "proposer";
+                node("P2")._proposalNo = 0;
+            })
+            .after(10, function () {
+                model().subtitle = '<h2>There are two proposers here.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>P1 initiates the Prepare phase, and ProposalNo is equal to 1.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(10, function () {
+                node("P1")._proposalNo += 1;
+                model().send(node("P1"), node("A"), {type: "RVREQ"}, function () {
+                    node("A")._proposalNo += 1;
+                    model().send(node("A"), node("P1"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                model().send(node("P1"), node("B"), {type: "RVREQ"}, function () {
+                    node("B")._proposalNo += 1;
+                    model().send(node("B"), node("P1"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                model().send(node("P1"), node("C"), {type: "RVREQ"}, function () {
+                    node("C")._proposalNo += 1;
+                    model().send(node("C"), node("P1"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>However, before P1 enters the Accept phase, P2 initiates the Prepare phase.</h2>'
+                    + '<h3 style="visibility:hidden; color: #a94442">But those Acceptors will reject the Prepare phase of P2 because P2.ProposalNo is not greater than 1</h3>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(300, function () {
+                node("P2")._proposalNo += 1;
+                model().send(node("P2"), node("A"), {type: "RVREQ"}, function () {
+                });
+                model().send(node("P2"), node("B"), {type: "RVREQ"}, function () {
+                });
+                model().send(node("P2"), node("C"), {type: "RVREQ"}, function () {
+                });
+                layout.invalidate();
+            })
+            .after(800, function () {
+                layout.fadeIn($(".subtitle h3"));
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>P2 will restart the Prepare phase and get the support of all Acceptors, and ProposalNo is equal to 2.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(500, function () {
+                node("P2")._proposalNo += 1;
+                model().send(node("P2"), node("A"), {type: "RVREQ"}, function () {
+                    node("A")._proposalNo += 1;
+                    model().send(node("A"), node("P2"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                model().send(node("P2"), node("B"), {type: "RVREQ"}, function () {
+                    node("B")._proposalNo += 1;
+                    model().send(node("B"), node("P2"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                model().send(node("P2"), node("C"), {type: "RVREQ"}, function () {
+                    node("C")._proposalNo += 1;
+                    model().send(node("C"), node("P2"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>Next, P1 enters the Accept phase, but because the ProposalNo of these Acceptors is equal to 2, P1 will be rejected.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(500, function () {
+                model().send(node("P1"), node("A"), {type: "AEREQ"}, function () {
+                });
+                model().send(node("P1"), node("B"), {type: "AEREQ"}, function () {
+                });
+                model().send(node("P1"), node("C"), {type: "AEREQ"}, function () {
+                });
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>So, P1 will restart the Prepare phase.</h2>' +
+                    '<h3 style="visibility:hidden;">Get the support of all Acceptors, and ProposalNo is equal to 3.</h3>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(500, function () {
+                node("P1")._proposalNo += 1;
+                model().send(node("P1"), node("A"), {type: "RVREQ"}, function () {
+                });
+                model().send(node("P1"), node("B"), {type: "RVREQ"}, function () {
+                });
+                model().send(node("P1"), node("C"), {type: "RVREQ"}, function () {
+                });
+                layout.invalidate();
+            })
+            .after(1500, function () {
+                node("P1")._proposalNo += 1;
+                model().send(node("P1"), node("A"), {type: "RVREQ"}, function () {
+                    node("A")._proposalNo += 1;
+                    model().send(node("A"), node("P1"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                model().send(node("P1"), node("B"), {type: "RVREQ"}, function () {
+                    node("B")._proposalNo += 1;
+                    model().send(node("B"), node("P1"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                model().send(node("P1"), node("C"), {type: "RVREQ"}, function () {
+                    node("C")._proposalNo += 1;
+                    model().send(node("C"), node("P1"), {type: "RVRSP"}, function () {
+                    });
+                    layout.invalidate();
+                });
+                layout.invalidate();
+            })
+            .after(1000, function () {
+                layout.fadeIn($(".subtitle h3"));
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>Then, P2 enters the Accept phase, and P2 will be rejected by all acceptors like P1.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(50, function () {
+                model().send(node("P2"), node("A"), {type: "AEREQ"}, function () {
+                });
+                model().send(node("P2"), node("B"), {type: "AEREQ"}, function () {
+                });
+                model().send(node("P2"), node("C"), {type: "AEREQ"}, function () {
+                });
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>So P2 needs to restart the Prepare phase, which will lead to the rejection of P1\'s Accept phase.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(100, wait).indefinite()
+
+            .after(10, function () {
+                frame.snapshot();
+                model().subtitle = '<h2>Move in circles<br/>In the end, there was no consensus on any proposal, as shown in the figure.</h2>'
+                    + model().controls.html();
+                layout.invalidate();
+            })
+            .after(200, function () {
+                frame.model().clients.removeAll();
+                frame.model().nodes.removeAll();
+                frame.model().title = '<image src="resources/livelock.png"/>';
                 layout.invalidate();
             })
             .after(100, wait).indefinite()
@@ -93,6 +282,7 @@ define(["../../../core/model/log_entry"], function (Proposal) {
             // Too many message interactions
             //------------------------------
             .after(500, function () {
+                frame.snapshot();
                 frame.model().clear();
                 frame.model().title = '<h2 style="visibility:visible">Too many message interactions</h2>'
                     + '<br/>' + frame.model().controls.html();
@@ -152,6 +342,7 @@ define(["../../../core/model/log_entry"], function (Proposal) {
             .after(100, wait).indefinite()
 
             .after(10, function () {
+                frame.snapshot();
                 model().subtitle = '<h2>To address this issue, we restate the meaning of the Prepare phase.</h2>'
                     + model().controls.html();
                 layout.invalidate();
@@ -159,6 +350,7 @@ define(["../../../core/model/log_entry"], function (Proposal) {
             .after(100, wait).indefinite()
 
             .after(10, function () {
+                frame.snapshot();
                 model().subtitle = '<h2>It can be found that ProposalNo is incremented every time a round of Prepare phase is performed.</h2>'
                     + '<h3 style="visibility:hidden; color: #a94442">In the case of no Proposal conflict, proposalNo is incremented continuously.</h3>'
                     + model().controls.html();
@@ -171,6 +363,7 @@ define(["../../../core/model/log_entry"], function (Proposal) {
             .after(100, wait).indefinite()
 
             .after(10, function () {
+                frame.snapshot();
                 model().subtitle = '<h3>In other words, if ProposalNo is not incremented in the Prepare phase, the ProposalNo promised by the Acceptor will not change, and the Accept phase can still be approved by the Acceptor.</h3>'
                     + model().controls.html();
                 layout.invalidate();
@@ -179,20 +372,16 @@ define(["../../../core/model/log_entry"], function (Proposal) {
             .after(10, function () {
                 model().stopRunPaxos();
                 frame.model().clear();
-                model().subtitle = '<h2>Then we imagine that after a round of prepare, subsequent negotiations can directly enter the Accept phase.</h2>'
+                model().subtitle = '<h2>Then we imagine that after a round of Prepare, subsequent negotiations can directly enter the Accept phase.</h2>'
                     + model().controls.html();
                 layout.invalidate();
             })
             .after(200, function () {
-                frame.model().title = '<image src="https://bkimg.cdn.bcebos.com/pic/63d0f703918fa0ec08facbbf5ac44eee3d6d55fb6d7f?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UyNzI=,g_7,xp_5,yp_5/format,f_auto"/>';
+                frame.model().title = '<image src="resources/direct-accept.png"/>';
                 layout.invalidate();
             })
             .after(100, wait).indefinite()
 
-
-
-
-            .after(100, wait).indefinite()
             .then(function () {
                 player.next();
             })
